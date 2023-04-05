0bebb5e2fabc4af4ed30f3ac54cc1abfe8815bde
Akmaral.K <kadesheva2023@gmail.com>
2023-04-05 15:57:09 +0500

    added an exercise 11

---
+++ 11/Program.cs
@@ -0,0 +1,7 @@
+﻿// 11 Напишите программу, которая выводит случайное трехзначное число
+// и удаляет вторую цифру этого числа:
+
+int num = new Random().Next(100, 1000);
+int result = (num/100)*10 + num%10;
+//Console.WriteLine($"{num} -> {result}"); alternative way to solve this issue
+Console.WriteLine($"{num} -> {num/100}{num%10}");
