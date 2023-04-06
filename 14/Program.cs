8829fa44007ee16aad9eec22eb812dcc6e90c42c
Akmaral.K <kadesheva2023@gmail.com>
2023-04-06 07:17:45 +0500

    added exercise 14

---
+++ 14/Program.cs
@@ -0,0 +1,12 @@
+﻿// 14. Напишите программу, которая принимает на вход число и проверяет
+// кратно ли оно одновременно 7 и 23
+// if ((a>b || a<c) && (x>b || x<c) || d == 5) &&
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if (num%7 == 0 && num % 23 == 0) 
+{
+    Console.WriteLine("Да");
+} else {
+    Console.WriteLine("Нет");
+}
